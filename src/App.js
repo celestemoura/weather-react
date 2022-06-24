bb8b3823f580e3f37b160b9c5dcacbd810b47f7e
Celeste Moura <37707126+celestemoura@users.noreply.github.com>
2022-06-24 17:21:58 +0200

    added link to GitHub repository

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <br />
+      <small>
+        <a
+          href="https://github.com/celestemoura/weather-react"
+          title="GitHub repository"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Open source code
+        </a>{" "}
+        by Celeste Moura
+      </small>
     </div>
   );
 }
