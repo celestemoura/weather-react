3addb582c10a1c022cf3fe0b430e6fa1e81018ef
Celeste Moura <37707126+celestemoura@users.noreply.github.com>
2022-06-25 16:51:00 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <SearchEngine />
-      <br />
-      <small>
-        <a
-          href="https://github.com/celestemoura/weather-react"
-          title="GitHub repository"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Open source code
-        </a>{" "}
-        by Celeste Moura
-      </small>
+      <div className="container">
+        <h1>Weather App</h1>
+        <SearchEngine />
+        <br />
+        <small>
+          <a
+            href="https://github.com/celestemoura/weather-react"
+            title="GitHub repository"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Open source code
+          </a>{" "}
+          by Celeste Moura
+        </small>
+      </div>
     </div>
   );
 }
