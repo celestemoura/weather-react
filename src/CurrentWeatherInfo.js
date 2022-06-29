3dceed060ab64a610bb40aa6ceb2c0e6c912fb84
Celeste Moura <37707126+celestemoura@users.noreply.github.com>
2022-06-29 17:15:43 +0200

    css refactoring

--- src/CurrentWeatherInfo.js
+++ src/CurrentWeatherInfo.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
-import "./CurrentWeatherInfo.css";
+import "./Weather.css";
 
 export default function CurrentWeatherInfo(props) {
   let fahrenheitTemperature = (props.data.temperature * 9) / 5 + 32;
   let minFahrenheitTemperature = (props.data.minTemp * 9) / 5 + 32;
   let maxFahrenheitTemperature = (props.data.maxTemp * 9) / 5 + 32;
-  let feelLikeFahrenheit = (props.data.feelsLike * 9) / 5 + 32;
+  let feelsLikeFahrenheit = (props.data.feelsLike * 9) / 5 + 32;
   const [unit, setUnit] = useState("celsius");
 
   function showFahrenheit(event) {
@@ -20,6 +20,7 @@ export default function CurrentWeatherInfo(props) {
 
   if (unit === "celsius") {
     return (
+      //Celsius display (Fahrenheit display below)
       <div className="CurrentWeatherInfo">
         <div className="row">
           <div className="col current-location">
@@ -55,8 +56,8 @@ export default function CurrentWeatherInfo(props) {
                   {Math.round(props.data.maxTemp)}°
                 </li>
                 <li>feels like {Math.round(props.data.feelsLike)}°</li>
-                <li>wind: {Math.round(props.data.wind)} km/h</li>
-                <li>{props.data.sky}</li>
+                <li>wind: {Math.round(props.data.windSpeed)} km/h</li>
+                <li>{props.data.skyConditions}</li>
               </ul>
             </span>
           </div>
@@ -65,6 +66,7 @@ export default function CurrentWeatherInfo(props) {
     );
   } else {
     return (
+      //Fahrenheit display
       <div className="CurrentWeatherInfo">
         <div className="row">
           <div className="col current-location">
@@ -99,9 +101,9 @@ export default function CurrentWeatherInfo(props) {
                   {Math.round(minFahrenheitTemperature)}°/
                   {Math.round(maxFahrenheitTemperature)}°
                 </li>
-                <li>feels like {Math.round(feelLikeFahrenheit)}°</li>
-                <li>wind: {Math.round(props.data.wind)} km/h</li>
-                <li>{props.data.sky}</li>
+                <li>feels like {Math.round(feelsLikeFahrenheit)}°</li>
+                <li>wind: {Math.round(props.data.windSpeed)} km/h</li>
+                <li>{props.data.skyConditions}</li>
               </ul>
             </span>
           </div>
@@ -110,57 +112,3 @@ export default function CurrentWeatherInfo(props) {
     );
   }
 }
-
-// export default function CurrentWeatherInfo(props) {
-//   // let celsiusTemperature = props.data.temperature;
-//   // let fahrenheitTemperature = (props.data.temperature * 9) / 5 + 32;
-//   const [unit, setUnit] = useState("celsius");
-
-//   if (unit === 'celsius') {
-
-//   }
-
-//   function showFahrenheit(event) {}
-
-//   return (
-//     <div className="CurrentWeatherInfo">
-//       <div className="row">
-//         <div className="col current-location">
-//           <span>
-//             {props.data.city}, {props.data.country}
-//           </span>
-//         </div>
-//       </div>
-//       <div className="row">
-//         <div className="col today">
-//           <span>
-//             <FormattedDate date={props.data.date} />
-//           </span>
-//           <span>
-//             <div className="current-temperature">
-//               {Math.round(props.data.temperature)}°
-//             </div>
-//             <div className="celsius-fahrenheit">
-//               <button className="active" title="Celsius">
-//                 °C
-//               </button>
-//               |<button title="Fahrenheit">°F</button>
-//             </div>
-//             <div className="main-icon">
-//               <WeatherIcon code={props.data.icon} />
-//             </div>
-//             <ul className="conditions">
-//               <li>
-//                 {Math.round(props.data.minTemp)}°/
-//                 {Math.round(props.data.maxTemp)}°
-//               </li>
-//               <li>feels like {Math.round(props.data.feelsLike)}°</li>
-//               <li>wind: {Math.round(props.data.wind)} km/h</li>
-//               <li>{props.data.sky}</li>
-//             </ul>
-//           </span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
