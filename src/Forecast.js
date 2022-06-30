e62f427267982bb01e62702df64be6a9e450fa0f
Celeste Moura <37707126+celestemoura@users.noreply.github.com>
2022-06-30 17:46:46 +0200

    added { Use Effect } as bug fix

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ForecastDay from "./ForecastDay";
 import axios from "axios";
 import "./Forecast.css";
@@ -11,6 +11,10 @@ export default function Forecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   if (loaded) {
     return (
